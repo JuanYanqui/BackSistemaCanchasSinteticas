815371a7e0e8c9f41a6a91851d47f44d98d79443
dverao97 <diego.vera.est@tecazuay.edu.ec>
2023-02-04 11:32:50 -0500

    .

--- src/main/java/com/sistema/canchas/controller/PagoReservaController.java
+++ src/main/java/com/sistema/canchas/controller/PagoReservaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pago")
+@RequestMapping("/pagoReserva")
 public class PagoReservaController {
     @Autowired
     PagoReservaService pagoReservaService;
