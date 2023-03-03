a58a4940bf7eacf1c63a811f4cecf6010ed2185d
Juan Yanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-03-03 17:51:04 -0500

    cambios para pruebs
    cambios para pruebs

--- src/main/java/com/sistema/canchas/controller/PersonaController.java
+++ src/main/java/com/sistema/canchas/controller/PersonaController.java
@@ -73,4 +73,41 @@ public class PersonaController {
     }
 */
 
+    @RequestMapping(value = "valced/{ced}", method = RequestMethod.GET)
+    @ResponseBody
+    @CrossOrigin
+    public boolean valcedu(@PathVariable String ced){
+        int suma=0;
+        int a[]=new int [ced.length()/2];
+        int b[]=new int [(ced.length()/2)];
+        int c=0;
+        int d=1;
+        for (int i = 0; i < ced.length()/2; i++) {
+            a[i]=Integer.parseInt(String.valueOf(ced.charAt(c)));
+            c=c+2;
+            if (i < (ced.length()/2)-1) {
+                b[i]=Integer.parseInt(String.valueOf(ced.charAt(d)));
+                d=d+2;
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            a[i]=a[i]*2;
+            if (a[i] >9){
+                a[i]=a[i]-9;
+            }
+            suma=suma+a[i]+b[i];
+        }
+        int auced=suma/10;
+        int dec=(auced+1)*10;
+        if ((dec - suma) == Integer.parseInt(String.valueOf(ced.charAt(ced.length()-1))))
+            return true;
+        else
+        if(suma%10==0 && ced.charAt(ced.length()-1)=='0'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
